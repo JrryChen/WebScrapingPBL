908585ecd33a676b12618f15614558018f99c80f
Jerry Chen <jzc23@pitt.edu>
2025-04-02 11:50:32 -0400

    creates a dictionary with the name and url of projects of https://github.com/collections/machine-learning

--- web_scraping.py
+++ web_scraping.py
@@ -1,3 +1,5 @@
+from sys import executable
+
 from selenium import webdriver # allow launching browser
 from selenium.webdriver.common.by import By # to allow search by parameters
 from selenium.webdriver.support.ui import WebDriverWait # to wait for the webpage to load
@@ -5,6 +7,24 @@ from selenium.webdriver.support import expected_conditions as EC # determine whe
 from selenium.common.exceptions import TimeoutException # TO handle timeout situation
 
 #Code to open new browser window
-webdriver = webdriver.ChromeOptions()
-webdriver.option.add_argument("--incognito")
-webdriver_path = "C:/Users/chess/PycharmProjects/WebScrapingPBL/"
+driver_option = webdriver.ChromeOptions()
+driver_option.add_argument(" — incognito")
+chromedriver_path = "C:/Users/chess/PycharmProjects/WebScrapingPBL/chromedriver-win64/chromedriver-win64/chromedriver.exe"
+
+def create_webdriver():
+ return webdriver.Chrome()
+
+if __name__ == "__main__":
+    browser = create_webdriver()
+    browser.get("https://github.com/collections/machine-learning")
+    # "//" finds all h1 elements, [@class=...] selects the ones with the attribute class = 'h3...'
+    projects = browser.find_elements(By.XPATH, "//h1[@class='h3 lh-condensed']")
+
+    project_list = {}
+    for project in projects:
+        proj_name = project.text
+        # ./ finds <a> elements under the project node, [0] gets the first <a> element, .get_attribute gets the attribute value
+        proj_url = project.find_elements(By.XPATH, "./a")[0].get_attribute("href")
+        project_list[proj_name] = proj_url
+
+    print(project_list)
